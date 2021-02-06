5d2cfb2b577eb7acbd31f1a5d12635ba9741ecce
Nat-Dev <nat.sherlockianz@gmail.com>
2021-02-06 15:26:45 +0700

    route to authen page at start

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:natkstatement/router.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,12 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'NatK Statement'),
+      routes: routes,
+      initialRoute: '/authen',
     );
   }
 }
---
+++ lib/router.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:natkstatement/widget/authen.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  '/authen': (BuildContext context) => Authen(),
+};
