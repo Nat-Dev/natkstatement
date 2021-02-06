1618c8ed02de847bd6899e23ec81d3739a5b294e
Nat-Dev <nat.sherlockianz@gmail.com>
2021-02-06 15:44:05 +0700

    add app logo to authen page

---
+++ lib/utility/my_style.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class MyStyle {
+  Widget showLogo() => Image.asset('images/logo.png');
+  MyStyle();
+}
--- lib/widget/authen.dart
+++ lib/widget/authen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:natkstatement/utility/my_style.dart';
 
 class Authen extends StatefulWidget {
   @override
@@ -6,11 +7,22 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  double screen;
+
   @override
   Widget build(BuildContext context) {
+    screen = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Authentication"),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: screen * 0.4,
+              child: MyStyle().showLogo(),
+            ),
+          ],
+        ),
       ),
     );
   }
