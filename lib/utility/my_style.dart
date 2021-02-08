b395d3222476e802d3d1dcd64de266e12b5bb9b0
Nat-Dev <nat.sherlockianz@gmail.com>
2021-02-08 12:53:34 +0700

    home page

--- lib/utility/my_style.dart
+++ lib/utility/my_style.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 
 class MyStyle {
+  Color themeColor = Color(0xff1976d2);
+  Color lightTheme = Color(0xff63a4ff);
+  Color darkTheme = Color(0xff004ba0);
+
   Widget showLogo() => Image.asset('images/logo.png');
+  Widget showBackground() => Image.asset('images/background.jpg');
+  Widget titleH1(String text) => Text(
+        text,
+        style: TextStyle(
+          fontSize: 28,
+          fontWeight: FontWeight.bold,
+          color: themeColor,
+        ),
+      );
+
   MyStyle();
 }
--- lib/widget/authen.dart
+++ lib/widget/authen.dart
@@ -8,22 +8,131 @@ class Authen extends StatefulWidget {
 
 class _AuthenState extends State<Authen> {
   double screen;
+  bool redEyeStatus = true;
 
   @override
   Widget build(BuildContext context) {
     screen = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              width: screen * 0.4,
-              child: MyStyle().showLogo(),
+      floatingActionButton: buildSignUp(),
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage("images/background.jpg"),
+          fit: BoxFit.cover,
+        )),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                buildLogo(),
+                MyStyle().titleH1("NatK Statement"),
+                buildUser(),
+                buildPassword(),
+                buildSignIn(),
+              ],
             ),
-          ],
+          ),
         ),
       ),
+    );
+  }
+
+  TextButton buildSignUp() {
+    return TextButton(
+      onPressed: () {
+        print("you click sign up");
+      },
+      child: Text(
+        "Sign Up",
+        style: TextStyle(color: MyStyle().lightTheme, fontSize: 16),
+      ),
+    );
+  }
+
+  Container buildSignIn() {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      width: screen * 0.8,
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text("Sign in"),
+        style: ElevatedButton.styleFrom(
+          primary: Colors.greenAccent.shade700,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container buildUser() {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      width: screen * 0.8,
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: "User:",
+          hintStyle: TextStyle(color: MyStyle().darkTheme),
+          prefixIcon: Icon(Icons.perm_identity),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25),
+            borderSide: BorderSide(color: MyStyle().darkTheme),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25),
+            borderSide: BorderSide(color: MyStyle().lightTheme),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container buildPassword() {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      width: screen * 0.8,
+      child: TextField(
+        obscureText: redEyeStatus,
+        decoration: InputDecoration(
+          hintText: "Password:",
+          hintStyle: TextStyle(color: MyStyle().darkTheme),
+          prefixIcon: Icon(Icons.lock_outline),
+          suffixIcon: IconButton(
+              icon: redEyeStatus
+                  ? Icon(Icons.remove_red_eye)
+                  : Icon(Icons.remove_red_eye_outlined),
+              onPressed: () {
+                setState(() {
+                  redEyeStatus = !redEyeStatus;
+                });
+              }),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25),
+            borderSide: BorderSide(color: MyStyle().darkTheme),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25),
+            borderSide: BorderSide(color: MyStyle().lightTheme),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container buildLogo() {
+    return Container(
+      width: screen * 0.4,
+      child: MyStyle().showLogo(),
+    );
+  }
+
+  Container buildBackground() {
+    return Container(
+      width: screen,
+      child: MyStyle().showBackground(),
     );
   }
 }
