18e859449c2bf841a8f11023a72b40cd778d5597
Nat-Dev <nat.sherlockianz@gmail.com>
2021-02-13 12:12:22 +0700

    auto sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:natkstatement/router.dart';
 
-void main() {
-  runApp(MyApp());
+String initialRoute = "/authen";
+Future<Null> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value) async {
+    // ignore: await_only_futures
+    await FirebaseAuth.instance.authStateChanges().listen((event) {
+      if (event != null) {
+        initialRoute = "/home";
+      }
+      runApp(MyApp());
+    });
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -10,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: routes,
-      initialRoute: '/authen',
+      initialRoute: initialRoute,
     );
   }
 }
